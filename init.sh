f7f0abfd53fe7808048caa0b3601767eae6af4b5
Bastiaan van Dreunen <bastiaan@superinteractive.com>
2015-08-15 15:25:46 +0200

    Further dev

--- init.sh
+++ init.sh
@@ -1,22 +1,86 @@
 #!/bin/bash
 #
 
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
-BLUE='\033[0;34m'
-NC='\033[0m' # No Color
+function _section {
 
-printf "\n\n${YELLOW}"
+  if [[ $1 == 'INFO' ]]; then
+    COLOR='\033[0m' # No Color
+  elif [[ $1 == 'INPUT' ]]; then
+    COLOR='\033[0;33m' # Yellow
+  elif [[ $1 == 'WARNING' ]]; then
+    COLOR='\033[0;31m' # Red
+  elif [[ $1 == 'SUCCESS' ]]; then
+    COLOR='\033[0;32m' # Green
+  elif [[ $1 == 'PROCESS' ]]; then
+    COLOR='\033[0;34m' # Blue
+  fi
+
+  printf "$COLOR"
+}
+
+# return 1 if global command line program installed, else 0
+# example
+# echo "node: $(program_is_installed node)"
+function program_is_installed {
+  # set to 1 initially
+  local return_=1
+  # set to 0 if not found
+  type $1 >/dev/null 2>&1 || { local return_=0; }
+  # return value
+  echo "$return_"
+}
+
+function prerequisites {
+
+  WARNING=0
+
+  if [[ 1 != $(program_is_installed npm) ]]; then
+    echo $(_section 'WARNING')
+    echo "PrintingPlate needs node and npm: https://nodejs.org/download/"
+    WARNING=1
+  fi
+
+  if [[ 1 != $(program_is_installed bower) ]]; then
+    echo $(_section 'WARNING')
+    echo "PrintingPlate needs Bower: http://bower.io/#install-bower"
+    WARNING=1
+  fi
+
+  if [[ 1 != $(program_is_installed gulp) ]]; then
+    echo $(_section 'WARNING')
+    echo "PrintingPlate needs Gulp: https://github.com/gulpjs/gulp/blob/master/docs/getting-started.md"
+    WARNING=1
+  fi
+
+  if [[ 1 != $(program_is_installed wp) ]]; then
+    echo $(_section 'WARNING')
+    echo "PrintingPlate needs WP_CLI: http://wp-cli.org"
+    WARNING=1
+  fi
+
+  if [[ 1 == $WARNING ]]; then
+    echo $(_section 'INFO')
+    printf "Please see to the issues above first and then run this script again.\n\n"
+    exit
+  fi
+
+}
+
+# Check dependencies
+prerequisites
+
+echo $(_section 'PROCESS')
 
 # Setup local environment
 cat 'etc/tools/init/ascii-art.txt'
+
+echo $(_section 'INFO')
 
 printf "\n\n\n${NC}Welcome to the ${BLUE}PrintingPlate${NC} setup.\n"
 
 printf "\nLet's start with some basic information for your new project.\n"
 
-printf "${YELLOW}"
+echo $(_section 'INPUT')
 
 # Project full name
 printf "Project full name (PrintingPlate): "
@@ -36,7 +100,8 @@ while [[ '' == $SHORTNAME ]]; do
   if [[ '' == $_SHORTNAME ]]; then
     SHORTNAME='printingplate'
   elif [[ $_SHORTNAME =~ [^a-zA-Z0-9\-] ]]; then
-    printf "${RED}* The shortname can only contain letters, numbers and dashes.${NC}\n"
+    echo $(_section 'WARNING')
+    printf "* The shortname can only contain letters, numbers and dashes.${NC}\n"
   else
     SHORTNAME=$_SHORTNAME
   fi
@@ -83,7 +148,11 @@ fi
 
 sed -i '' "s#<PPdescription>#$DESCRIPTION#g" "app/www/content/themes/$SHORTNAME/style.css"
 
+echo $(_section 'INFO')
+
 printf "\n${NC}Awesome! Now let's configure your local development environment.${YELLOW}\n"
+
+echo $(_section 'INPUT')
 
 # Copy the env template
 cp 'etc/tools/init/env.tpl' '.env'
@@ -147,11 +216,13 @@ done
 
 sed -i '' "s#<PPlocalurl>#$LOCAL_URL#g" '.env'
 
-printf "\n${NC}Great, now PrintingPlate will now start building some files for your environment.\n"
+echo $(_section 'INFO')
+printf "\nGreat, now PrintingPlate will now start building some files for your environment.\n"
 
 # Auto-generate salts for wp-config.php
 
-printf "${BLUE}Download salts for wp-config.php......"
+echo $(_section 'PROCESS')
+printf "Download salts for wp-config.php......"
 
 cp 'etc/tools/init/wp-config.tpl' 'app/www/wp-config.php'
 
@@ -161,31 +232,64 @@ salts_e=$(echo $salts | sed 's/[|&]/\\&/g')
 
 sed -i '' "s|<PPsalts>|$salts_e|" "app/www/wp-config.php"
 
-printf "${GREEN}Done\n\n"
+echo $(_section 'SUCCESS')
+printf "Done\n\n"
 
-printf "${BLUE}Installing PHP dependencies with composer......."
+echo $(_section 'PROCESS')
+printf "Installing PHP dependencies with composer......."
 
-composer -q install
+error=$(composer -q install)
+if [[ 0 != $error ]]; then
+  echo $(_section 'WARNING')
+  echo $error
+fi
 
-printf "${GREEN}Done\n"
+echo $(_section 'SUCCESS')
+printf "Done\n"
 
-printf "${BLUE}Installing Javascript dependencies with Bower......."
+echo $(_section 'PROCESS')
+printf "Installing Javascript dependencies with Bower......."
 
-bower install
+error=$(bower -l=error install)
+if [[ 0 != $error ]]; then
+  echo $(_section 'WARNING')
+  echo $error
+fi
 
-printf "${GREEN}Done\n"
+echo $(_section 'SUCCESS')
+printf "Done\n"
 
-printf "${BLUE}Installing npm tools......."
+echo $(_section 'PROCESS')
+printf "Installing npm tools......."
 
-npm install
+error=$(npm --loglevel=error install)
+if [[ 0 != $error ]]; then
+  echo $(_section 'WARNING')
+  echo "Error occurred: "
+  echo $error
+fi
 
-printf "${GREEN}Done\n"
+echo $(_section 'SUCCESS')
+printf "Done\n"
 
-printf "${BLUE}Building assets with gulp......."
+echo $(_section 'PROCESS')
+printf "Building assets with gulp......."
 
-gulp
+error=$(gulp)
+if [[ 0 != $error ]]; then
+  echo $(_section 'WARNING')
+  echo $error
+fi
 
-printf "${GREEN}Done\n"
+echo $(_section 'SUCCESS')
+printf "Done\n"
 
+echo $(_section 'PROCESS')
+printf "Instaling WordPress......."
 
+error=$(wp --path=app/www/wp --url=${LOCAL_URL} core install)
+if [[ 0 != $error ]]; then
+  echo $(_section 'WARNING')
+  echo $error
+fi
 
