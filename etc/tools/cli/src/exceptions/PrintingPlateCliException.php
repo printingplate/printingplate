2dc5484b0174eb85a488e98b814e70c3be2e8730
Bastiaan van Dreunen <bastiaan@superinteractive.com>
2016-01-11 10:14:02 +0100

    Add cli tool

---
+++ etc/tools/cli/src/exceptions/PrintingPlateCliException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace PrintingPlate\PrintingPlateCli\Exceptions;
+
+/**
+ * Class DiffbotException
+ * @package Swader\Diffbot\Exceptions
+ */
+class PrintingPlateCliException extends \Exception
+{
+  
+}
--- etc/tools/init/style.tpl
+++
@@ -1,7 +0,0 @@
-/*
-Theme Name: <PPname>
-Theme URI: <PPuri>
-Description: <PPdescription>
-Author: <PPauthor>
-Version: 0.1
-*/
